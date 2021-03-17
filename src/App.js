5106339fa1d21c7677a071ace905497d562adbdc
NarentHub <53078891+Narent-hub@users.noreply.github.com>
2021-03-17 09:38:14 +0100

    Fetching data from react to the api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
+import { BrowserRouter, Route } from 'react-router-dom'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+
 import './App.css'
 import RegisterForm from './components/registerForm/registerForm'
 
 function App() {
   return (
-    <div className='container-md'>
+    <BrowserRouter>
       <div className='row'>
-        <div className='left col-sm-12 col-md-12 col-lg-6'>
-          <h5>
-            If you want to use our eVoting platform, all you need to do is to
-            fill the fields required to the right section of this page
-          </h5>
-          <h5>
-            Mention: <br></br> Your credentials remain private to us!
-          </h5>
-          <img src='./views/Register/voting-proxy-evoting.jpg' alt=''></img>
-        </div>
-        <RegisterForm />
+        <Header />
       </div>
-    </div>
+
+      <Route exact path='/register' component={RegisterForm} />
+
+      {/* <div className='left col-sm-12 col-md-12 col-lg-6'>
+            <h5>
+              If you want to use our eVoting platform, all you need to do is to
+              fill the fields required to the right section of this page
+            </h5>
+            <h5>
+              Mention: <br></br> Your credentials remain private to us!
+            </h5>
+            <img src='./views/Register/voting-proxy-evoting.jpg' alt=''></img>
+          </div>
+          <RegisterForm /> */}
+
+      <div className='row'>
+        <Footer />
+      </div>
+    </BrowserRouter>
   )
 }
 
--- src/components/registerForm/registerForm.js
+++ src/components/registerForm/registerForm.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Formik } from 'formik'
 import * as Yup from 'yup'
+import axios from 'axios'
 
 import Error from '../Error/Error'
 import './registerForm.css'
@@ -23,133 +24,157 @@ const validationSchema = Yup.object().shape({
     .min(8, 'Password is too short - should be 8 chars minimum.'),
 })
 
-function RegisterForm() {
-  return (
-    <div className='right col-sm-12 col-md-12 col-lg-6'>
-      <h4>Welcome to our eVoting platform's registration form</h4>
-      <Formik
-        initialValues={{
-          f_name: '',
-          l_name: '',
-          email: '',
-          password: '',
-        }}
-        validationSchema={validationSchema}
-        onSubmit={(values, { setSubmitting, resetForm }) => {
-          setTimeout(() => {
-            resetForm()
-            setSubmitting(false)
-          }, 500)
-        }}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          isSubmitting,
-        }) => (
-          <form onSubmit={handleSubmit}>
-            <div className='row row-fields row-1'>
-              <div className='formInput form-group mobile-col col-sm-12 col-md-6 col-lg-12'>
-                <input
-                  name='f_name'
-                  type='text'
-                  placeholder='First Name'
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.f_name}
-                  className={
-                    touched.f_name && errors.f_name ? 'has-error' : null
-                  }
-                ></input>
-                <Error touched={touched.f_name} message={errors.f_name} />
-              </div>
+class RegisterForm extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      gender: '',
+      birthdate: '',
+      role: '',
+    }
+  }
 
-              <div className='formInput form-group mobile-col col-sm-12 col-md-6 col-lg-12'>
-                <input
-                  name='l_name'
-                  type='text'
-                  placeholder='Last Name'
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.l_name}
-                  className={
-                    touched.l_name && errors.l_name ? 'has-error' : null
-                  }
-                ></input>
-                <Error touched={touched.l_name} message={errors.l_name} />
-              </div>
+  componentDidMount() {
+    axios
+      .post('/http://127.0.0.1:8000/api/user', this.state)
+      .then((response) => {
+        return console.log(response)
+      })
+  }
+  render() {
+    return (
+      <div className='right col-sm-12 col-md-12 col-lg-6'>
+        <h4>Welcome to our eVoting platform's registration form</h4>
+        <Formik
+          initialValues={{
+            f_name: '',
+            l_name: '',
+            email: '',
+            password: '',
+          }}
+          validationSchema={validationSchema}
+          onSubmit={(values, { setSubmitting, resetForm }) => {
+            setTimeout(() => {
+              resetForm()
+              setSubmitting(false)
+            }, 500)
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <div className='row row-fields row-1'>
+                <div className='formInput form-group mobile-col col-sm-12 col-md-6 col-lg-12'>
+                  <input
+                    name='f_name'
+                    type='text'
+                    placeholder='First Name'
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.f_name}
+                    className={
+                      touched.f_name && errors.f_name ? 'has-error' : null
+                    }
+                  ></input>
+                  <Error touched={touched.f_name} message={errors.f_name} />
+                </div>
 
-              <div className='formInput form-group mobile-col col-sm-12 col-md-6 col-lg-12'>
-                <input
-                  name='email'
-                  type='email'
-                  placeholder='Email'
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.email}
-                  className={touched.email && errors.email ? 'has-error' : null}
-                ></input>
-                <Error touched={touched.email} message={errors.email} />
-              </div>
+                <div className='formInput form-group mobile-col col-sm-12 col-md-6 col-lg-12'>
+                  <input
+                    name='l_name'
+                    type='text'
+                    placeholder='Last Name'
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.l_name}
+                    className={
+                      touched.l_name && errors.l_name ? 'has-error' : null
+                    }
+                  ></input>
+                  <Error touched={touched.l_name} message={errors.l_name} />
+                </div>
 
-              <div className='formInput form-group mobile-col col-sm-12 col-md-6 col-lg-12'>
-                <input
-                  name='password'
-                  type='password'
-                  placeholder='Password'
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.password}
-                  className={
-                    touched.password && errors.password ? 'has-error' : null
-                  }
-                ></input>
-                <Error touched={touched.password} message={errors.password} />
-              </div>
+                <div className='formInput form-group mobile-col col-sm-12 col-md-6 col-lg-12'>
+                  <input
+                    name='email'
+                    type='email'
+                    placeholder='Email'
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.email}
+                    className={
+                      touched.email && errors.email ? 'has-error' : null
+                    }
+                  ></input>
+                  <Error touched={touched.email} message={errors.email} />
+                </div>
 
-              <div className='formInput form-group mobile-col col-sm-12 col-md-6 col-lg-12'>
-                <label for='gender'>Gender:</label>
-                <br></br>
-                <select name='gender' id='gender' required>
-                  <option value='female'>--Choose your gender--</option>
-                  <option value='female'>Female</option>
-                  <option value='male'>Male</option>
-                </select>
-              </div>
+                <div className='formInput form-group mobile-col col-sm-12 col-md-6 col-lg-12'>
+                  <input
+                    name='password'
+                    type='password'
+                    placeholder='Password'
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.password}
+                    className={
+                      touched.password && errors.password ? 'has-error' : null
+                    }
+                  ></input>
+                  <Error touched={touched.password} message={errors.password} />
+                </div>
 
-              <div className='formInput form-group mobile-col col-sm-12 col-md-6 col-lg-12'>
-                <label for='bDay'>Birthday</label>
-                <br></br>
-                <input name='dateofbirth' type='date'></input>
-              </div>
+                <div className='formInput form-group mobile-col col-sm-12 col-md-6 col-lg-12'>
+                  <label for='gender'>Gender:</label>
+                  <br></br>
+                  <select name='gender' id='gender' required>
+                    <option value='female'>--Choose your gender--</option>
+                    <option value='female'>Female</option>
+                    <option value='male'>Male</option>
+                  </select>
+                </div>
 
-              <div className='formInput form-group mobile-col col-sm-12 col-md-6 col-lg-12'>
-                <label for='gender'>You are registering as:</label>
-                <br></br>
-                <select name='role' id='role' required>
-                  <option value='role'>--Choose your role--</option>
-                  <option value='Voter'>Creator/Organizer</option>
-                  <option value='Creator'>Voter/Participant</option>
-                </select>
+                <div className='formInput form-group mobile-col col-sm-12 col-md-6 col-lg-12'>
+                  <label for='bDay'>Birthday</label>
+                  <br></br>
+                  <input name='dateofbirth' type='date'></input>
+                </div>
+
+                <div className='formInput form-group mobile-col col-sm-12 col-md-6 col-lg-12'>
+                  <label for='gender'>You are registering as:</label>
+                  <br></br>
+                  <select name='role' id='role' required>
+                    <option value='role'>--Choose your role--</option>
+                    <option value='Voter'>Creator/Organizer</option>
+                    <option value='Creator'>Voter/Participant</option>
+                  </select>
+                </div>
+                <button
+                  type='submit'
+                  label='send'
+                  className='btn submit-btn btn-outline-dark btn pseudoBtn'
+                  disabled={isSubmitting}
+                >
+                  Submit
+                </button>
               </div>
-              <button
-                type='submit'
-                label='send'
-                className='btn submit-btn btn-outline-dark btn pseudoBtn'
-                disabled={isSubmitting}
-              >
-                Submit
-              </button>
-            </div>
-          </form>
-        )}
-      </Formik>
-    </div>
-  )
+            </form>
+          )}
+        </Formik>
+      </div>
+    )
+  }
 }
 
 export default RegisterForm
