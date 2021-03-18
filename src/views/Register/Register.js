5f6e08f6521c209a8085e785c4cd72ce5cafdeea
NarentHub <53078891+Narent-hub@users.noreply.github.com>
2021-03-18 23:20:58 +0100

    latest

--- src/views/Register/Register.js
+++ src/views/Register/Register.js
@@ -1,25 +1,21 @@
 import React from 'react'
 import './Register.css'
 import RegisterForm from '../../components/registerForm/registerForm'
+import voting from './voting-proxy-evoting.jpg'
 
 function Register() {
   return (
     <div className='container-md'>
       <div className='row'>
         <div className='left col-sm-12 col-md-12 col-lg-6'>
-          <h5>
-            If you want to use our eVoting platform, all you need to do is to
-            fill the fields required to the right section of this page
-          </h5>
+          <h3>One step ahead of eVoting/create poll</h3>
+          <img src={voting} alt='voting img'></img>
+
           <h5>
             Mention: <br></br> Your credentials remain private to us!
           </h5>
-          <img src='./views/Register/voting-proxy-evoting.jpg' alt=''></img>
         </div>
-        <div className='right col-sm-12 col-md-12 col-lg-6'>
-          <h4>Welcome to our eVoting platform's registration form</h4>
-          <RegisterForm />
-        </div>
+        <RegisterForm />
       </div>
     </div>
   )
