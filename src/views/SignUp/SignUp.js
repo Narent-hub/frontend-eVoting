f0d7047f7d7ac8577ec3450f0d958380e2727076
NarentHub <53078891+Narent-hub@users.noreply.github.com>
2021-04-08 12:09:33 +0200

    scss

--- src/views/SignUp/SignUp.js
+++ src/views/SignUp/SignUp.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import '../../App.css';
+import React from 'react'
+import '../../App.scss'
 
-export default function SignUp()
-{
-    return <h1 className='sign-up'>SIGN UP</h1>;
+export default function SignUp() {
+  return <h1 className='sign-up'>SIGN UP</h1>
 }
